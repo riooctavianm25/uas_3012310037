50651ab65960fe0c90d6ebd2e2af186d62fdad80
viann0422-hub <email_github_kamu>
2026-01-18 03:43:23 +0700

    update destination

--- lib/data/model/destinations.dart
+++ lib/data/model/destinations.dart
@@ -1,27 +1,41 @@
 class TourismPlace {
+  final int id;
   final String name;
   final String description;
-  final String imageUrl;
+  final String address;
+  final String image;
+  final double rating;
 
   TourismPlace({
+    required this.id,
     required this.name,
     required this.description,
-    required this.imageUrl,
+    required this.address,
+    required this.image,
+    required this.rating,
   });
 
   factory TourismPlace.fromJson(Map<String, dynamic> json) {
     return TourismPlace(
-      name: json['name'] ?? '',
+      id: json['id'] ?? 0,
+      name: json['name'] ?? 'No Name',
       description: json['description'] ?? '',
-      imageUrl: json['imageUrl'] ?? '',
+      address: json['address'] ?? 'Unknown Location',
+      image: json['cover_image'] != null
+          ? 'http://10.0.2.2:8000/storage/${json['cover_image']}'
+          : 'https://via.placeholder.com/150',
+      rating: (json['avg_rating'] ?? 0).toDouble(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'name': name,
       'description': description,
-      'imageUrl': imageUrl,
+      'address': address,
+      'cover_image': image,
+      'avg_rating': rating,
     };
   }
 }
