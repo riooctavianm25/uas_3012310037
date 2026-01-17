71c4ffcafb29cd9f15b06d08725394162967e56d
viann0422-hub <email_github_kamu>
2026-01-17 13:30:58 +0700

    update auth repo

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,26 +1,30 @@
 import 'package:http/http.dart' as http;
 import 'package:uas_3012310037/data/service/httpservice.dart';
+import 'package:uas_3012310037/data/usecase/request/register_request.dart';
+
 
 class AuthRepository {
-  final HttpService _httpService = HttpService();
-  
-  Future<http.Response> register(Map<String, dynamic> body) async {
-    final response = await _httpService.post('register', body);
+  final HttpService httpService;
+
+  AuthRepository({required this.httpService});
+
+  Future<http.Response> register(RegisterRequest request) async {
+    final response = await httpService.post('register', request.toMap());
     return response;
   }
 
   Future<http.Response> login(Map<String, dynamic> body) async {
-    final response = await _httpService.post('login', body);
+    final response = await httpService.post('login', body);
     return response;
   }
 
   Future<http.Response> logout() async {
-    final response = await _httpService.post('logout', {});
+    final response = await httpService.post('logout', {});
     return response;
   }
 
   Future<http.Response> getUserProfile() async {
-    final response = await _httpService.get('profile', {});
+    final response = await httpService.get('profile', {});
     return response;
   }
 }
